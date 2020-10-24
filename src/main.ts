fe2996a1075a82a10eb11c3dc26a6bccd1a9747c
Nic Cope <negz@rk0n.org>
2020-10-23 17:17:03 -0700

    Add an action that creates semver git tags
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,51 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as semver from 'semver';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token = core.getInput('token')
+    const tag = core.getInput('version')
+    const msg = core.getInput('message')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (semver.valid(tag) == null) {
+      core.setFailed(`Tag ${tag} does not appear to be a valid semantic version`)
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const owner = github.context.repo.owner
+    const repo = github.context.repo.repo
+    const sha = github.context.sha
+    const ref = `refs/tags/${tag}`
+
+    const client = github.getOctokit(token)
+
+    const tag_rsp = await client.git.createTag({
+      owner: owner,
+      repo: repo,
+      tag: tag,
+      message: msg,
+      object: sha,
+      type: "commit",
+    })
+    if (tag_rsp.status !== 201) {
+      core.setFailed(`Failed to create tag object (status=${tag_rsp.status})`)
+      return
+    }
+
+    const ref_rsp = await client.git.createRef({
+      owner: owner,
+      repo: repo,
+      ref: ref,
+      sha: sha,
+    })
+    if (ref_rsp.status !== 201) {
+      core.setFailed(`Failed to create tag ref (status=${tag_rsp.status})`)
+      return
+    }
+
+    core.info(`Tagged ${sha} as ${tag}`)
+
   } catch (error) {
     core.setFailed(error.message)
   }

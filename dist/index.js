39bae1e0932567a58c20dea5a1a0d18358503320
Nic Cope <negz@rk0n.org>
2020-10-27 15:39:35 -0700

    Create ref of the tag's sha, not the commit's
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- dist/index.js
+++ dist/index.js
@@ -1835,41 +1835,23 @@ const semver = __importStar(__webpack_require__(383));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const token = core.getInput('token');
             const tag = core.getInput('version');
-            const msg = core.getInput('message');
             if (semver.valid(tag) == null) {
                 core.setFailed(`Tag ${tag} does not appear to be a valid semantic version`);
                 return;
             }
-            const owner = github.context.repo.owner;
-            const repo = github.context.repo.repo;
-            const sha = github.context.sha;
-            const ref = `refs/tags/${tag}`;
-            const client = github.getOctokit(token);
-            const tag_rsp = yield client.git.createTag({
-                owner,
-                repo,
-                tag,
-                message: msg,
-                object: sha,
-                type: 'commit'
-            });
+            const client = github.getOctokit(core.getInput('token'));
+            const tag_rsp = yield client.git.createTag(Object.assign(Object.assign({}, github.context.repo), { tag, message: core.getInput('message'), object: github.context.sha, type: 'commit' }));
             if (tag_rsp.status !== 201) {
                 core.setFailed(`Failed to create tag object (status=${tag_rsp.status})`);
                 return;
             }
-            const ref_rsp = yield client.git.createRef({
-                owner,
-                repo,
-                ref,
-                sha
-            });
+            const ref_rsp = yield client.git.createRef(Object.assign(Object.assign({}, github.context.repo), { ref: `refs/tags/${tag}`, sha: tag_rsp.data.sha }));
             if (ref_rsp.status !== 201) {
-                core.setFailed(`Failed to create tag ref (status=${tag_rsp.status})`);
+                core.setFailed(`Failed to create tag ref(status = ${tag_rsp.status})`);
                 return;
             }
-            core.info(`Tagged ${sha} as ${tag}`);
+            core.info(`Tagged ${tag_rsp.data.sha} as ${tag}`);
         }
         catch (error) {
             core.setFailed(error.message);
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import * as semver from 'semver'
 
 async function run(): Promise<void> {
   try {
-    const token = core.getInput('token')
     const tag = core.getInput('version')
-    const msg = core.getInput('message')
-
     if (semver.valid(tag) == null) {
       core.setFailed(
         `Tag ${tag} does not appear to be a valid semantic version`
@@ -15,19 +12,13 @@ async function run(): Promise<void> {
       return
     }
 
-    const owner = github.context.repo.owner
-    const repo = github.context.repo.repo
-    const sha = github.context.sha
-    const ref = `refs/tags/${tag}`
-
-    const client = github.getOctokit(token)
+    const client = github.getOctokit(core.getInput('token'))
 
     const tag_rsp = await client.git.createTag({
-      owner,
-      repo,
+      ...github.context.repo,
       tag,
-      message: msg,
-      object: sha,
+      message: core.getInput('message'),
+      object: github.context.sha,
       type: 'commit'
     })
     if (tag_rsp.status !== 201) {
@@ -36,17 +27,16 @@ async function run(): Promise<void> {
     }
 
     const ref_rsp = await client.git.createRef({
-      owner,
-      repo,
-      ref,
-      sha
+      ...github.context.repo,
+      ref: `refs/tags/${tag}`,
+      sha: tag_rsp.data.sha
     })
     if (ref_rsp.status !== 201) {
-      core.setFailed(`Failed to create tag ref (status=${tag_rsp.status})`)
+      core.setFailed(`Failed to create tag ref(status = ${tag_rsp.status})`)
       return
     }
 
-    core.info(`Tagged ${sha} as ${tag}`)
+    core.info(`Tagged ${tag_rsp.data.sha} as ${tag}`)
   } catch (error) {
     core.setFailed(error.message)
   }
